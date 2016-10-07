1a07135a5d6fb7aef25d850fe660caa6ea7d30d8
ihachani <ihachaniuploads@gmail.com>
2016-10-07 17:51:50 +0100

    Added oldWay Example to Main

--- src/main/java/com/ihachani/lambda_reference/Main.java
+++ src/main/java/com/ihachani/lambda_reference/Main.java
@@ -3,10 +3,18 @@ package com.ihachani.lambda_reference;
 public class Main {
 
     public static void main(String[] argv) {
-
+        oldWay();
     }
 
 
+    public static void oldWay() {
+        Speaker speaker = new Speaker(new Speech() {
+            public void speak(String speech) {
+                System.out.println("Introduction " + speech);
+            }
+        });
+        speaker.talk();
+    }
 
 
 }
